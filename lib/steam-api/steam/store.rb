8d6b9b2cb431cf880ead7c39e450911e030da603
Vlad <pacMakaveli90@gmail.com>
2021-03-30 10:21:02 +0100

    fix(): language is optional, but without it, we may get games in the language the developer specified, which may not be english

--- lib/steam-api/steam/store.rb
+++ lib/steam-api/steam/store.rb
@@ -6,12 +6,27 @@ module Steam
     # Get App Details
     # @param String appid The UUID of the Steam Application
     # @see https://wiki.teamfortress.com/wiki/User:RJackson/StorefrontAPI#appdetails
-    def self.app_details(appid)
-      response = client.get 'appdetails', params: { appids: appid }
+    def self.app_details(appid, language: 'English')
+      response = client.get('appdetails', params: {
+        appids: appid,
+        l: language
+      })
+
       response
     end
 
-    private
+    def self.price_overview(appids = [], currency: 'en', language: 'English')
+      response = client.get('appdetails', params: {
+        filters: 'price_overview',
+        appids: appids.join(','),
+        cc: currency,
+        l: language
+      }
+
+      response
+    end
+
+  private
 
     def self.client
       build_client '', base_url: Steam::Helpers::STORE_API_BASE_URL
